5c2b8af0d05825fd11de1de297ce1de6e89ae4ae
Godfred Fiave <fiavegp@gmail.com>
2025-08-05 22:21:36 +0000

    added task three

---
+++ Interfaces/Interfaces/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    // Interface definition
+    interface IMovable
+    {
+        void Move();
+    }
+
+    // Car class implementing IMovable
+    class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    // Bicycle class implementing IMovable
+    class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMovable car = new Car();
+            IMovable bicycle = new Bicycle();
+
+            Console.WriteLine("Vehicle Movements:");
+            car.Move();
+            bicycle.Move();
+
+            Console.ReadLine();
+        }
+    }
+}
