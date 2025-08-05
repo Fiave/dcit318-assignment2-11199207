5d3efefc6ab79575141953d8b565f84dbe072bd0
Godfred Fiave <fiavegp@gmail.com>
2025-08-05 22:01:05 +0000

    added inheritance and method overriding app

---
+++ Inheritance and Method Overriding/Inheritance and Method Overriding/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Method_Overriding
+{
+    class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Random made sound.");
+        }
+    }
+
+    // Derived class for Dog
+    class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    // Derived class for cat
+    class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal genericAnimal = new Animal();
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            Console.WriteLine("Animal sound:");
+            genericAnimal.MakeSound();
+
+            Console.WriteLine("\nDog sound:");
+            dog.MakeSound();
+
+            Console.WriteLine("\nCat sound:");
+            cat.MakeSound();
+
+            Console.ReadLine();
+        }
+    }
+}
